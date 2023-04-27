dc8d6c8b43f5ed6e20b689c8f4d11b14747f4182
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2023-04-27 23:14:07 +0200

    all windows overrides to the bottom

--- shell/wezterm/wezterm.lua
+++ shell/wezterm/wezterm.lua
@@ -1,16 +1,12 @@
 local wezterm = require "wezterm"
-
 local helpers = require "helpers"
 
 local config = wezterm.config_builder()
 
 config.color_scheme = helpers.scheme_for_appearance(helpers.get_appearance())
-config.font = wezterm.font('Source Code Pro', { weight = "Regular" })
-config.font_size =
-    wezterm.target_triple == 'x86_64-pc-windows-msvc'
-    and 13
-    or 14
 
+config.font = wezterm.font('Source Code Pro', { weight = "Regular" })
+config.font_size = 14
 config.line_height = 1.15
 
 config.audible_bell = "Disabled"
@@ -22,8 +18,8 @@ config.window_padding = {
   bottom = 10,
 }
 
-
 if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
+  config.font_size = 13
   config.default_cwd = "D:\\Users\\atj"
   config.default_prog = { 'cmd.exe', '/k' }
 end
