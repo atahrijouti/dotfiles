9f94fa30398adff2819bfc444f031adbc93aca5f
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2023-04-27 21:22:45 +0200

    add windows installer

---
+++ install.bat
@@ -0,0 +1,6 @@
+@echo off
+
+
+mklink /d C:\Users\atj\AppData\Local\clink D:\Users\atj\playground\git\dotfiles\windows\clink
+mklink /d C:\Users\atj\AppData\Local\nvim D:\Users\atj\playground\git\dotfiles\ides\nvim
+mklink /d C:\Users\atj\.config\wezterm D:\Users\atj\playground\git\dotfiles\shell\wezterm
