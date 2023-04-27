7915fd311ccb9173a840e8d8d7ce222ea481ad31
Abderrahmane TAHRI JOUTI <atahrijouti@paypal.com>
2023-04-27 18:14:51 +0200

    reduce repetition a little bit

--- install.sh
+++ install.sh
@@ -2,8 +2,15 @@
 
 DOTFILES_DIR=$(dirname -- "$0";)
 
+link() {
+    local source="$DOTFILES_DIR/$1"
+    local target="$2"
+
+    ln -s "$source" "$target"
+}
+
 # link
-ln -s "$DOTFILES_DIR/shell/.zshrc" "$HOME/.zshrc"
-ln -s "$DOTFILES_DIR/shell/starship/starship.toml" "$HOME/.config/starship.toml"
-ln -s "$DOTFILES_DIR/shell/wezterm" "$HOME/.config/wezterm"
-ln -s "$DOTFILES_DIR/ides/nvim" "$HOME/.config/nvim"
+link "shell/.zshrc"                     "$HOME/.zshrc"
+link "shell/starship/starship.toml"     "$HOME/.config/starship.toml"
+link "shell/wezterm"                    "$HOME/.config/wezterm"
+link "ides/nvim"                        "$HOME/.config/nvim"
