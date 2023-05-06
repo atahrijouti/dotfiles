70c399486a3286524b633f0612d037d8da3bafc6
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2023-05-06 18:45:48 +0200

    bigger font-size on x240

--- shell/wezterm/wezterm.lua
+++ shell/wezterm/wezterm.lua
@@ -23,6 +23,9 @@ config.hide_tab_bar_if_only_one_tab = true
 
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
 	config.font_size = 10
+	if wezterm.hostname() == "x240" then
+		config.font_size = 11
+	end
 	config.default_prog = { "cmd.exe", "/k" }
 end
 
