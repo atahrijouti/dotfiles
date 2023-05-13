533a9e056e12aff53a4fcf5560991f736f8bc8f6
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2023-05-13 19:24:58 +0200

    config color scheme in the right place

--- ides/nvim/lua/plugins/colorscheme.lua
+++ ides/nvim/lua/plugins/colorscheme.lua
@@ -2,9 +2,13 @@ return {
   {
     "folke/tokyonight.nvim",
     lazy = false,
+    priority = 1000,
     opts = {
       style = "storm",
       light_style = "day",
     },
+    config = function()
+      vim.cmd([[colorscheme tokyonight]])
+    end,
   },
 }
