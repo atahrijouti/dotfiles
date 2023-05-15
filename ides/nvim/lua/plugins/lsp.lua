cd68519b2ad9eae1fc9c3b6b0cf793d9208557ab
Abderrahmane TAHRI JOUTI <atahrijouti@paypal.com>
2023-05-15 11:26:35 +0200

    shift things to the right places

--- ides/nvim/lua/plugins/lsp.lua
+++ ides/nvim/lua/plugins/lsp.lua
@@ -1,14 +1,6 @@
-local on_attach = function(_, bufnr)
-  vim.api.nvim_buf_create_user_command(bufnr, "Format", function()
-    vim.lsp.buf.format()
-  end, { desc = "Format current buffer with LSP" })
-end
-
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-
 return {
   -- { "folke/neoconf.nvim", cmd = "Neoconf", config = true },
-  -- { "folke/neodev.nvim", opts = { experimental = { pathStrict = true } } },
+  { "folke/neodev.nvim", opts = { experimental = { pathStrict = true } } },
   {
     "neovim/nvim-lspconfig",
     event = { "BufReadPre", "BufNewFile" },
@@ -58,16 +50,15 @@ return {
       masonLspconfig.setup({
         ensure_installed = vim.tbl_keys(servers),
         automatic_installation = true,
-      })
-
-      masonLspconfig.setup_handlers({
-        function(server_name)
-          lspconfig[server_name].setup({
-            capabilities = capabilities,
-            on_attach = on_attach,
-            settings = servers[server_name],
-          })
-        end,
+        handlers = {
+          function(server_name)
+            lspconfig[server_name].setup({
+              -- capabilities = capabilities,
+              on_attach = on_attach,
+              settings = servers[server_name],
+            })
+          end,
+        },
       })
     end,
   },
