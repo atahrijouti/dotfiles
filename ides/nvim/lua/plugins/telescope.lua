3ddbf69f6be8a76797934b78668f72ea998860ae
Abderrahmane TAHRI JOUTI <atahrijouti@paypal.com>
2023-05-16 15:49:33 +0200

    Telescope: Prompt up, order ascending

--- ides/nvim/lua/plugins/telescope.lua
+++ ides/nvim/lua/plugins/telescope.lua
@@ -4,10 +4,41 @@ return {
     tag = "0.1.1",
     cmd = "Telescope",
     dependencies = {
-      -- "nvim-lua/plenary.nvim",
+      "nvim-lua/plenary.nvim",
       "telescope-fzf-native.nvim",
       -- "project.nvim",
     },
+    opts = function()
+      local actions = require("telescope.actions")
+      return {
+        defaults = {
+          path_display = { "truncate" },
+          sorting_strategy = "ascending",
+          layout_config = {
+            horizontal = {
+              prompt_position = "top",
+              preview_width = 0.55,
+            },
+            vertical = {
+              mirror = false,
+            },
+            width = 0.87,
+            height = 0.80,
+            preview_cutoff = 120,
+          },
+
+          mappings = {
+            i = {
+              ["<C-n>"] = actions.cycle_history_next,
+              ["<C-p>"] = actions.cycle_history_prev,
+              ["<C-j>"] = actions.move_selection_next,
+              ["<C-k>"] = actions.move_selection_previous,
+            },
+            n = { ["q"] = actions.close },
+          },
+        },
+      }
+    end,
   },
   {
     "nvim-telescope/telescope-fzf-native.nvim",
