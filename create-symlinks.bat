84c77eb455d7b1a3b86361b4aa570e2939b2bfee
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2023-05-18 13:03:47 +0200

    vscode keyboard shortcuts symlink

--- create-symlinks.bat
+++ create-symlinks.bat
@@ -2,9 +2,14 @@
 
 if not exist "%USERPROFILE%\.config" mkdir "%USERPROFILE%\.config"
 
-mklink /d "%USERPROFILE%\AppData\Local\clink" "%USERPROFILE%\playground\git\dotfiles\windows\clink"
-mklink /d "%USERPROFILE%\AppData\Local\nvim" "%USERPROFILE%\playground\git\dotfiles\ides\nvim"
-mklink /d "%USERPROFILE%\.config\wezterm" "%USERPROFILE%\playground\git\dotfiles\shell\wezterm"
-mklink "%USERPROFILE%\.config\starship.toml" "%USERPROFILE%\playground\git\dotfiles\shell\starship\starship.toml"
+set DOTFILES="%USERPROFILE%\playground\git\dotfiles"
+
+mklink /d "%USERPROFILE%\AppData\Local\clink" "%DOTFILES%\windows\clink"
+mklink /d "%USERPROFILE%\AppData\Local\nvim" "%DOTFILES%\ides\nvim"
+mklink /d "%USERPROFILE%\.config\wezterm" "%DOTFILES%\shell\wezterm"
+mklink "%USERPROFILE%\.config\starship.toml" "%DOTFILES%\shell\starship\starship.toml"
+
+REM mklink "%USERPROFILE%\AppData\Roaming\Code\keybindings.json" "%DOTFILES%\ides\vscode\keybindings.json"
+
 
 "C:\Program Files (x86)\clink\\clink.bat" autorun install -- -q
