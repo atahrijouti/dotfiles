ebdf6525748c46f2de77c228a7ff98a0394a97d7
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2023-05-20 22:12:51 +0200

    nvim cmp, vs keys, windows install

--- ides/nvim/lua/plugins/cmp.lua
+++ ides/nvim/lua/plugins/cmp.lua
@@ -1,58 +1,117 @@
 return {
-  -- Autocompletion
-  "hrsh7th/nvim-cmp",
-  dependencies = {
-    "hrsh7th/cmp-nvim-lsp",
-    "L3MON4D3/LuaSnip",
-    "saadparwaiz1/cmp_luasnip",
-    "rafamadriz/friendly-snippets",
-  },
+	{
+		"hrsh7th/nvim-cmp",
+		event = "InsertEnter",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-emoji",
+			"saadparwaiz1/cmp_luasnip",
+		},
+		opts = function()
+			local has_words_before = function()
+				unpack = unpack or table.unpack
+				local line, col = table.unpack(vim.api.nvim_win_get_cursor(0))
+				return col ~= 0
+					and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+			end
 
-  config = function()
-    local cmp = require("cmp")
-    local luasnip = require("luasnip")
-    require("luasnip.loaders.from_vscode").lazy_load()
-    luasnip.config.setup({})
+			local luasnip = require("luasnip")
+			local cmp = require("cmp")
 
-    cmp.setup({
-      snippet = {
-        expand = function(args)
-          luasnip.lsp_expand(args.body)
-        end,
-      },
-      mapping = cmp.mapping.preset.insert({
-        ["<C-n>"] = cmp.mapping.select_next_item(),
-        ["<C-p>"] = cmp.mapping.select_prev_item(),
-        ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"] = cmp.mapping.complete({}),
-        ["<CR>"] = cmp.mapping.confirm({
-          behavior = cmp.ConfirmBehavior.Replace,
-          select = true,
-        }),
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-          elseif luasnip.expand_or_locally_jumpable() then
-            luasnip.expand_or_jump()
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif luasnip.locally_jumpable(-1) then
-            luasnip.jump(-1)
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-      }),
-      sources = {
-        { name = "nvim_lsp" },
-        { name = "luasnip" },
-      },
-    })
-  end,
+			local border_opts = {
+				border = "single",
+				winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:Visual,Search:None",
+			}
+
+			return {
+				-- preselect = cmp.PreselectMode.None,
+				window = {
+					completion = cmp.config.window.bordered(border_opts),
+					documentation = cmp.config.window.bordered(border_opts),
+				},
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp", priority = 1000 },
+					{ name = "luasnip", priority = 750 },
+					{ name = "buffer", priority = 500 },
+					{ name = "path", priority = 250 },
+					{ name = "emoji", insert = true, priority = 150 },
+				}),
+				snippet = {
+					expand = function(args)
+						require("luasnip").lsp_expand(args.body)
+					end,
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<Up>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+					["<Down>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+					["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+					["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+					["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+					["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+					["<C-u>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
+					["<C-d>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
+					["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+					["<C-y>"] = cmp.config.disable,
+					["<C-e>"] = cmp.mapping({ i = cmp.mapping.abort(), c = cmp.mapping.close() }),
+					["<CR>"] = cmp.mapping.confirm({ select = false }),
+					["<Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
+						elseif luasnip.expand_or_jumpable() then
+							luasnip.expand_or_jump()
+						elseif has_words_before() then
+							cmp.complete()
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+					["<S-Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item()
+						elseif luasnip.jumpable(-1) then
+							luasnip.jump(-1)
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+				}),
+			}
+		end,
+	},
+
+	{
+		"L3MON4D3/LuaSnip",
+		event = "InsertEnter",
+		dependencies = { "rafamadriz/friendly-snippets" },
+		build = (not jit.os:find("Windows")) and "make install_jsregexp" or nil,
+		keys = {
+			{
+				"<C-l>",
+				function()
+					require("luasnip").expand_or_jump()
+				end,
+				mode = { "i", "s" },
+			},
+		},
+		opts = {
+			-- Don't store snippet history for less overhead
+			history = false,
+			-- Event on which to check for exiting a snippet's region
+			region_check_events = "InsertEnter",
+			delete_check_events = "InsertLeave",
+			ft_func = function()
+				return vim.split(vim.bo.filetype, ".", { plain = true })
+			end,
+		},
+		config = function(_, opts)
+			require("luasnip").setup(opts)
+			require("luasnip.loaders.from_vscode").lazy_load()
+			require("luasnip.loaders.from_lua").load({ paths = "./snippets" })
+			vim.api.nvim_create_user_command("LuaSnipEdit", function()
+				require("luasnip.loaders.from_lua").edit_snippet_files()
+			end, {})
+		end,
+	},
 }
--- install-windows.bat
+++ install-windows.bat
@@ -70,3 +70,4 @@ winget install -e --id KeePassXCTeam.KeePassXC
 winget install -e --id Dropbox.Dropbox
 winget install -e --id Parsec.Parsec
 winget install -e --id Valve.Steam
+winget install -e --id Spotify.Spotify
