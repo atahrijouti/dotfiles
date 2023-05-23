62c599768796f39a4b992a5f09546b8d7656077e
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2023-05-23 15:31:24 +0200

    whitespace

--- ides/nvim/init.lua
+++ ides/nvim/init.lua
@@ -3,10 +3,10 @@ require("config.lazy")
 
 vim.api.nvim_create_augroup("lazy", { clear = true })
 vim.api.nvim_create_autocmd("User", {
-  group = "lazy",
-  pattern = "VeryLazy",
-  callback = function()
-    require("config.autocommands")
-    require("config.keymaps")
-  end,
+	group = "lazy",
+	pattern = "VeryLazy",
+	callback = function()
+		require("config.autocommands")
+		require("config.keymaps")
+	end,
 })
