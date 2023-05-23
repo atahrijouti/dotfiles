9cb3c6545971b01bf47097c3f0bcd8f90e2c2dc2
Abderrahmane TAHRI JOUTI <atahrijouti@paypal.com>
2023-05-23 17:25:22 +0200

    dressing

--- ides/nvim/lua/plugins/ui.lua
+++ ides/nvim/lua/plugins/ui.lua
@@ -1,71 +1,69 @@
 return {
-	{
-		"lewis6991/gitsigns.nvim",
-		event = "BufReadPost",
-		opts = {
-			signs = {
-				add = { text = "▎" },
-				change = { text = "▏" },
-				delete = { text = "󰐊" },
-				topdelete = { text = "󰐊" },
-				changedelete = { text = "╏" },
-				untracked = { text = "┆" },
-			},
-		},
-		config = function(_, opts)
-			require("gitsigns").setup(opts)
-		end,
-	},
-	{
-		"rcarriga/nvim-notify",
-	},
-	{
-		"NvChad/nvim-colorizer.lua",
-		event = "User",
-		cmd = {
-			"ColorizerToggle",
-			"ColorizerAttachToBuffer",
-			"ColorizerDetachFromBuffer",
-			"ColorizerReloadAllBuffers",
-		},
-		-- opts = { user_default_options = { names = false } },
-	},
-	{
-		"nvim-neo-tree/neo-tree.nvim",
-		cmd = "Neotree",
-		opts = {
-			window = {
-				position = "left",
-				width = 30,
-			},
-			filesystem = {
-				filtered_items = {
-					visible = false,
-					hide_dotfiles = false,
-					hide_gitignored = false,
-					hide_hidden = false,
-				},
-				follow_current_file = true,
-			},
-		},
-	},
-	{
-		"MunifTanjim/nui.nvim",
-	},
-	{
-		"folke/noice.nvim",
-		config = function()
-			require("noice").setup({
-				-- add any options here
-			})
-		end,
-		requires = {
-			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-			"MunifTanjim/nui.nvim",
-			-- OPTIONAL:
-			--   `nvim-notify` is only needed, if you want to use the notification view.
-			--   If not available, we use `mini` as the fallback
-			"rcarriga/nvim-notify",
-		},
-	},
+  {
+    "lewis6991/gitsigns.nvim",
+    event = "BufReadPost",
+    opts = {
+      signs = {
+        add = { text = "▎" },
+        change = { text = "▏" },
+        delete = { text = "󰐊" },
+        topdelete = { text = "󰐊" },
+        changedelete = { text = "╏" },
+        untracked = { text = "┆" },
+      },
+    },
+    config = function(_, opts)
+      require("gitsigns").setup(opts)
+    end,
+  },
+  {
+    "rcarriga/nvim-notify",
+  },
+  {
+    "NvChad/nvim-colorizer.lua",
+    event = "User",
+    cmd = {
+      "ColorizerToggle",
+      "ColorizerAttachToBuffer",
+      "ColorizerDetachFromBuffer",
+      "ColorizerReloadAllBuffers",
+    },
+    -- opts = { user_default_options = { names = false } },
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    cmd = "Neotree",
+    opts = {
+      window = {
+        position = "left",
+        width = 30,
+      },
+      filesystem = {
+        filtered_items = {
+          visible = false,
+          hide_dotfiles = false,
+          hide_gitignored = false,
+          hide_hidden = false,
+        },
+        follow_current_file = true,
+      },
+    },
+  },
+  {
+    "MunifTanjim/nui.nvim",
+  },
+  -- {
+  --   "folke/noice.nvim",
+  --   requires = {
+  --     "MunifTanjim/nui.nvim",
+  --     "rcarriga/nvim-notify",
+  --   },
+  --   config = function()
+  --     require("noice").setup({})
+  --   end,
+  -- },
+  {
+    "stevearc/dressing.nvim",
+    opts = {},
+  },
 }
