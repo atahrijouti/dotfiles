5f2679ea7af5894c451e43b6103514bb3486a1fa
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2023-05-24 17:14:15 +0200

    my lua line

--- ides/nvim/lua/plugins/lua-line.lua
+++ ides/nvim/lua/plugins/lua-line.lua
@@ -5,6 +5,8 @@ return {
     return {
       options = {
         theme = "auto",
+        section_separators = "",
+        component_separators = "",
         globalstatus = true,
         disabled_filetypes = { statusline = { "dashboard", "alpha" } },
       },
@@ -15,47 +17,48 @@ return {
           {
             "diagnostics",
           },
-          { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
-          { "filename", path = 1, symbols = { modified = "  ", readonly = "", unnamed = "" } },
-          -- stylua: ignore
-          {
-            function() return require("nvim-navic").get_location() end,
-            cond = function() return package.loaded["nvim-navic"] and require("nvim-navic").is_available() end,
-          },
         },
         lualine_x = {
-          -- stylua: ignore
           {
-            function() return require("noice").api.status.command.get() end,
-            cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
-          },
-          -- stylua: ignore
-          {
-            function() return require("noice").api.status.mode.get() end,
-            cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
-          },
-          -- stylua: ignore
-          {
-            function() return "  " .. require("dap").status() end,
-            cond = function () return package.loaded["dap"] and require("dap").status() ~= "" end,
-          },
-          {
-            require("lazy.status").updates,
-            cond = require("lazy.status").has_updates,
+            "encoding",
           },
           {
             "diff",
           },
+          {
+            function()
+              return require("noice").api.status.command.get()
+            end,
+            cond = function()
+              return package.loaded["noice"] and require("noice").api.status.command.has()
+            end,
+          },
+          {
+            function()
+              return require("noice").api.status.mode.get()
+            end,
+            cond = function()
+              return package.loaded["noice"] and require("noice").api.status.mode.has()
+            end,
+          },
         },
-        lualine_y = {
-          { "progress", separator = " ", padding = { left = 1, right = 0 } },
+        lualine_y = {},
+        lualine_z = {
           { "location", padding = { left = 0, right = 1 } },
         },
-        lualine_z = {
-          function()
-            return " " .. os.date("%R")
-          end,
+      },
+      winbar = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {
+          {
+            "filename",
+            path = 1,
+          },
         },
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {},
       },
       extensions = { "neo-tree", "lazy" },
     }
