3cb4ac821fe7b0f4f5bb5c69c95b7cbde3cb493f
Abderrahmane TAHRI JOUTI <atahrijouti@paypal.com>
2023-05-31 12:22:41 +0200

    borrow nvim lua types from neodev

--- ides/nvim/init.lua
+++ ides/nvim/init.lua
@@ -1,12 +1,13 @@
 require("config.options")
+---@diagnostic disable-next-line: different-requires
 require("config.lazy")
 
 vim.api.nvim_create_augroup("lazy", { clear = true })
 vim.api.nvim_create_autocmd("User", {
-	group = "lazy",
-	pattern = "VeryLazy",
-	callback = function()
-		require("config.autocommands")
-		require("config.keymaps")
-	end,
+  group = "lazy",
+  pattern = "VeryLazy",
+  callback = function()
+    require("config.autocommands")
+    require("config.keymaps")
+  end,
 })
