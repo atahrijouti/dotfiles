1ba62c2bc3e44f11bbe2496f51c2a4b5d26ec26f
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2023-07-02 21:07:24 +0200

    add helix config to dotfiles

--- create-symlinks.bat
+++ create-symlinks.bat
@@ -7,13 +7,18 @@ set DOTFILES="%USERPROFILE%\playground\git\dotfiles"
 mklink /d "%USERPROFILE%\AppData\Local\clink" "%DOTFILES%\windows\clink"
 mklink /d "%USERPROFILE%\AppData\Local\nvim" "%DOTFILES%\ides\nvim"
 mklink /d "%USERPROFILE%\.config\wezterm" "%DOTFILES%\shell\wezterm"
-
 mklink "%USERPROFILE%\.config\starship.toml" "%DOTFILES%\shell\starship\starship.toml"
 
-mklink "%USERPROFILE%\.bash_profile" "%USERPROFILE%\playground\git\dotfiles\windows\msys_shell\.bash_profile"
-mklink "%USERPROFILE%\.bash_functions" "%USERPROFILE%\playground\git\dotfiles\windows\msys_shell\.bash_functions"
-mklink "%USERPROFILE%\.bash_aliases" "%USERPROFILE%\playground\git\dotfiles\windows\msys_shell\.bash_aliases"
+REM Helix
+mklink "%USERPROFILE%\AppData\Roaming\helix\config.toml" "%DOTFILES%\ides\helix\config.toml"
+mklink "%USERPROFILE%\AppData\Roaming\helix\languages.toml" "%DOTFILES%\ides\helix\languages.toml"
 
+REM MSYS Bash
+mklink "%USERPROFILE%\.bash_profile" "%DOTFILES%\windows\msys_shell\.bash_profile"
+mklink "%USERPROFILE%\.bash_functions" "%DOTFILES%\windows\msys_shell\.bash_functions"
+mklink "%USERPROFILE%\.bash_aliases" "%DOTFILES%\windows\msys_shell\.bash_aliases"
+
+REM VSCode
 REM mklink "%USERPROFILE%\AppData\Roaming\Code\keybindings.json" "%DOTFILES%\ides\vscode\keybindings.json"
 
 
