9ee900df652d6930fa0f670b205253a1db3b59c6
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2023-07-02 19:25:00 +0200

    add msys bash config files

--- create-symlinks.bat
+++ create-symlinks.bat
@@ -7,7 +7,12 @@ set DOTFILES="%USERPROFILE%\playground\git\dotfiles"
 mklink /d "%USERPROFILE%\AppData\Local\clink" "%DOTFILES%\windows\clink"
 mklink /d "%USERPROFILE%\AppData\Local\nvim" "%DOTFILES%\ides\nvim"
 mklink /d "%USERPROFILE%\.config\wezterm" "%DOTFILES%\shell\wezterm"
+
 mklink "%USERPROFILE%\.config\starship.toml" "%DOTFILES%\shell\starship\starship.toml"
+
+mklink "%USERPROFILE%\.bash_profile" "%USERPROFILE%\playground\git\dotfiles\windows\msys_shell\.bash_profile"
+mklink "%USERPROFILE%\.bash_functions" "%USERPROFILE%\playground\git\dotfiles\windows\msys_shell\.bash_functions"
+mklink "%USERPROFILE%\.bash_aliases" "%USERPROFILE%\playground\git\dotfiles\windows\msys_shell\.bash_aliases"
 
 REM mklink "%USERPROFILE%\AppData\Roaming\Code\keybindings.json" "%DOTFILES%\ides\vscode\keybindings.json"
 
