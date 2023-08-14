5a13d8c56416dd7607ceb203dbfccc2c376c10e0
Abderrahmane TAHRI JOUTI <atahrijouti@paypal.com>
2023-08-14 11:19:41 +0200

    wezterm - mac native keymap overrides

--- shell/wezterm/overrides/os/apple-silicon.lua
+++ shell/wezterm/overrides/os/apple-silicon.lua
@@ -1,7 +1,13 @@
 local M = {}
 
+local wezterm = require("wezterm")
+local act = wezterm.action
+
 M.run = function(config)
-  print("from apple-silicon")
+  table.insert(config.keys, { key = "q", mods = "CMD", action = act.QuitApplication })
+  table.insert(config.keys, { key = "h", mods = "CMD", action = act.HideApplication })
+  table.insert(config.keys, { key = "c", mods = "CMD", action = act.CopyTo("Clipboard") })
+  table.insert(config.keys, { key = "v", mods = "CMD", action = act.PasteFrom("Clipboard") })
   return config
 end
 
