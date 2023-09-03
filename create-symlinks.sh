6f338d958e2202499890b33c754e7dcf2c7bbcb6
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2023-09-03 10:27:37 +0200

    linux, helix

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -1,11 +1,9 @@
-#!/bin/zsh
-
-DOTFILES_DIR=$(dirname -- "$0";)
+DOTFILES_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 link() {
     local source="$DOTFILES_DIR/$1"
     local target="$2"
-
+    echo "linking $source -> $target"
     ln -s "$source" "$target"
 }
 
@@ -15,3 +13,4 @@ link "shell/starship/starship.toml"     "$HOME/.config/starship.toml"
 link "shell/wezterm"                    "$HOME/.config/wezterm"
 link "ides/nvim"                        "$HOME/.config/nvim"
 link "ides/lf"                          "$HOME/.config/lf"
+link "ides/helix"                       "$HOME/.config/helix"
