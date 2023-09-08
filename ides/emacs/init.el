f3105f2cf64e175a4524f425c7f7d1cb374807ce
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2023-09-08 22:21:34 +0200

    emacs - add all sorts of packages

--- ides/emacs/init.el
+++ ides/emacs/init.el
@@ -3,14 +3,4 @@
 (require 'elpaca-installer)
 
 (require 'atj-options)
-
-(use-package all-the-icons
-  :ensure t
-  :if (display-graphic-p))
-
-(use-package all-the-icons-dired
-  :hook (dired-mode . (lambda () (all-the-icons-dired-mode t))))
-
-(use-package evil
-  :init
-  (evil-mode))
+(require 'atj-packages)
--- ides/emacs/lisp/atj-options.el
+++ ides/emacs/lisp/atj-options.el
@@ -1,10 +1,13 @@
+(set-face-attribute 'default nil
+  :font "JetBrainsMono NFM")
+
 (setq ring-bell-function #'ignore)
 (setq inhibit-startup-screen t)
 (setq initial-scratch-message nil)
 (setq make-backup-files nil)
 (setq default-directory "~/")
 
-(set-face-attribute 'default nil
-  :font "JetBrainsMono NFM")
+(global-auto-revert-mode t)  ;; Automatically show changes if the file has changed
+(global-display-line-numbers-mode 1) ;; Display line numbers
 
 (provide 'atj-options)
