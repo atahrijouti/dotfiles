34c22a398419a7cdb79b2cfa2ee7b35b17014db5
Abderrahmane TAHRI JOUTI <atahrijouti@paypal.com>
2023-09-11 15:46:35 +0200

    emacs - dashboard with project.el

--- ides/emacs/lisp/atj-project.el
+++ ides/emacs/lisp/atj-project.el
@@ -1,8 +1,37 @@
 ;; Projects
-(use-package projectile
-  :diminish
+
+;(use-package projectile
+;  :diminish
+;  :config
+;  (projectile-mode 1))
+
+(require 'project)
+
+
+(use-package dashboard
+  :ensure t 
+  :init
+  (setq initial-buffer-choice 'dashboard-open)
+  (setq dashboard-set-heading-icons t)
+  (setq dashboard-projects-backend 'project-el)
+  (setq dashboard-set-file-icons t)
+  (setq dashboard-startup-banner 'logo)
+  (setq dashboard-center-content t)
+  (setq dashboard-items'(
+        (projects . 5 )
+        (recents . 5)
+        (bookmarks . 3)
+        (registers . 3)
+    )
+  )
+  :custom 
+    (dashboard-modify-heading-icons '(
+        (recents . "file-text")
+        (bookmarks . "book"))
+    )
   :config
-  (projectile-mode 1))
+    (dashboard-setup-startup-hook)
+)
 
 (use-package ripgrep)
 
--- ides/emacs/lisp/atj-ui.el
+++ ides/emacs/lisp/atj-ui.el
@@ -12,34 +12,6 @@
 (use-package all-the-icons-dired
   :hook (dired-mode . (lambda () (all-the-icons-dired-mode t))))
 
-
-(use-package dashboard
-  :after projectile
-  :ensure t 
-  :init
-  (setq initial-buffer-choice 'dashboard-open)
-  (setq dashboard-set-heading-icons t)
-  (setq dashboard-set-file-icons t)
-  (setq dashboard-startup-banner 'logo)
-  (setq dashboard-center-content t)
-  (setq dashboard-items'(
-        (projects . 5 )
-        (recents . 5)
-        (bookmarks . 3)
-        (registers . 3)
-    )
-  )
-  :custom 
-    (dashboard-modify-heading-icons '(
-        (recents . "file-text")
-        (bookmarks . "book"))
-    )
-  :config
-    (dashboard-setup-startup-hook)
-)
-
-
-
 ;; WhichKey
 (use-package which-key
   :diminish
