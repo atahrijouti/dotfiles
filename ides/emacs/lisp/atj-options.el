2372da6e475be61ecd376d3aa2cb1d16220e0b99
Abderrahmane TAHRI JOUTI <atahrijouti@paypal.com>
2023-09-11 16:36:17 +0200

    disable auto-save

--- ides/emacs/lisp/atj-options.el
+++ ides/emacs/lisp/atj-options.el
@@ -4,12 +4,12 @@
 (setq ring-bell-function #'ignore)
 (setq inhibit-startup-screen t)
 (setq initial-scratch-message nil)
-(setq make-backup-files nil)
 (setq default-directory "~/")
+
+(setq make-backup-files nil)
+(setq auto-save-default nil)
 
 (global-auto-revert-mode t)  ;; Automatically show changes if the file has changed
 (global-display-line-numbers-mode 1) ;; Display line numbers
 
 (provide 'atj-options)
-
-;;;; heheheh
