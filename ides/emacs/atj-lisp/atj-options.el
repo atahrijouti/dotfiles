645c222b1f0d04f2f8c873ea3089dc771b4e52c3
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2023-09-22 11:46:56 +0200

    emacs - put backups in their directory

--- ides/emacs/atj-lisp/atj-options.el
+++ ides/emacs/atj-lisp/atj-options.el
@@ -3,12 +3,15 @@
 (setq initial-scratch-message nil)
 (setq default-directory "~/")
 
+;; Backups
 (setq make-backup-files nil)
 (setq auto-save-default nil)
+(setq backup-directory-alist '((".*" . "~/.emacs.d/emacs-backup")))
+(setq create-lockfiles nil)
+
 
 ;; Automatically show changes if the file has changed
 (global-auto-revert-mode t)
-
 
 (global-display-line-numbers-mode 1)
 
