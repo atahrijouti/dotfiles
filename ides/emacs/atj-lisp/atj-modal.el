3613483efbc3cdc538c67f0d715f654138a8718c
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2023-09-24 17:35:21 +0200

    emacs - autoload ryo

--- ides/emacs/atj-lisp/atj-modal.el
+++ ides/emacs/atj-lisp/atj-modal.el
@@ -1,25 +1,36 @@
-(setq viper-mode 0)
-(require 'viper)
-
 (use-package ryo-modal
-  :commands ryo-modal-mode
-  :bind ("C-c SPC" . ryo-modal-mode)
+  ;; :commands ryo-modal-mode
+  ;; :bind ("C-c SPC" . ryo-modal-mode)
   :config
-  (ryo-modal-keys
-   ("," ryo-modal-repeat)
-   ("q" ryo-modal-mode)
+  (setq viper-mode 0)
+  (require 'viper)
 
+  (defun ryo-enter () "Enter normal mode" (interactive) (ryo-modal-mode 1))
+   (add-hook 'prog-mode-hook #'ryo-enter)
+   (add-hook 'text-mode-hook #'ryo-enter)
+
+   ;; use keykoard to be able to use jk
+   ;; (global-set-key (kbd "jk") #'ryo-enter)
+
+   (ryo-modal-keys
+   (:mc-all t)
+   
+   ;; Basic movement
    ("h" backward-char)
    ("j" next-line)
    ("k" previous-line)
    ("l" forward-char)
 
+   ("," ryo-modal-repeat)
+   ("q" ryo-modal-mode)
+
    ("w" viper-forward-word)
    ("b" viper-backward-word)
   )
+
+
+  ;; C-u {number} 
   (ryo-modal-keys
-   ;; First argument to ryo-modal-keys may be a list of keywords.
-   ;; These keywords will be applied to all keybindings.
    (:norepeat t)
    ("0" "M-0")
    ("1" "M-1")
@@ -30,9 +41,8 @@
    ("6" "M-6")
    ("7" "M-7")
    ("8" "M-8")
-   ("9" "M-9"))
-  :init
-    (ryo-modal-mode)
-  )
+   ("9" "M-9")
+   )
+)
 
 (provide 'atj-modal)
