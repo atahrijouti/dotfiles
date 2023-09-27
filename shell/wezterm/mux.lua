bd7dbd1d615fd067bd11441e614e2a9c6802124e
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2023-09-27 10:23:09 +0200

    wezterm - one step further towards full mux

--- shell/wezterm/mux.lua
+++ shell/wezterm/mux.lua
@@ -3,20 +3,34 @@ local wezterm = require("wezterm")
 local M = {}
 
 M.openLazyGitdown = function(window, _pane)
+  wezterm.log_info("----------------------------------")
   local panes = window:mux_window():active_tab():panes()
 
-  local targetPaneId = nil
+  local targetPane = nil
+
   local info = {}
   for _, pane in ipairs(panes) do
     local title = pane:get_title():gsub("C:\\Windows\\system32\\cmd.exe%s+-%s", "")
     local paneId = pane:pane_id()
     if title:match(nocase("lazygit")) then
-      targetPaneId = paneId
+      targetPane = pane
     end
     table.insert(info, paneId .. " 👉 " .. title)
   end
+
   wezterm.log_info(info)
-  wezterm.log_info(targetPaneId)
+  wezterm.log_info(targetPane and targetPane:pane_id() .. " - ✅" or "🛑")
+
+
+  if targetPane then
+    return
+  else
+    targetPane = _pane:split({ direction = "Bottom" })
+  end
+
+  targetPane:send_paste("lazygit" .. "\n")
+
+  wezterm.log_info("----------------------------------")
 end
 
 return M
