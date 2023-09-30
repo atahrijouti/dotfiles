2fa66a0b3d68f2ef756247d30e5a44bdeec2a0fc
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2023-09-30 17:22:46 +0200

    wezterm - find toggle programs without title

--- shell/wezterm/mux.lua
+++ shell/wezterm/mux.lua
@@ -10,27 +10,42 @@ M.openLfLeft = function()
   return M.toggleProgramInPaneSplit({ program = "lf", direction = "Left", size = 0.2 })
 end
 
+local getTargetProgramPaneDict = function(pane)
+  local tab = pane:tab()
+  local tabId = tab:tab_id() .. ""
+  local window = tab:window()
+  local windowId = window:window_id() .. ""
+
+  if wezterm.GLOBAL.targetPane == nil then
+    wezterm.GLOBAL.targetPane = {}
+  end
+  if wezterm.GLOBAL.targetPane[windowId] == nil then
+    wezterm.GLOBAL.targetPane[windowId] = {}
+  end
+  if wezterm.GLOBAL.targetPane[windowId][tabId] == nil then
+    wezterm.GLOBAL.targetPane[windowId][tabId] = {}
+  end
+
+  return wezterm.GLOBAL.targetPane[windowId][tabId]
+end
+
+---@param o {program: string, direction?: string, size?: number}
 M.toggleProgramInPaneSplit = function(o)
-  return function(window, _pane)
-    local panes = window:mux_window():active_tab():panes()
+  return function(window, pane)
+    local targetDict = getTargetProgramPaneDict(pane)
+    local programPaneId = targetDict[o.program]
 
-    local targetPane = nil
-
-    for _, pane in ipairs(panes) do
-      local title = pane:get_title():gsub("C:\\Windows\\system32\\cmd.exe%s+-%s", "")
-      -- local paneId = pane:pane_id()
-      if title:match(nocase(o.program)) then
-        targetPane = pane
-      end
-    end
-
-    if targetPane then
-      targetPane:activate()
-      window:perform_action(wezterm.action.CloseCurrentPane { confirm = false }, targetPane)
-      return
+    if programPaneId then
+      local programPane = wezterm.mux.get_pane(programPaneId)
+      programPane:activate()
+      window:perform_action(wezterm.action.CloseCurrentPane { confirm = false }, programPane)
+      targetDict[o.program] = nil
     else
-      targetPane = _pane:split({ direction = o.direction and o.direction or "Bottom", size = o.size and o.size or 0.5 })
-      targetPane:send_paste(o.program .. "\n")
+      local newPane = pane:split({
+        direction = o.direction and o.direction or "Bottom", size = o.size and o.size or 0.5
+      })
+      newPane:send_paste(o.program .. "\n")
+      targetDict[o.program] = newPane:pane_id()
     end
   end
 end
