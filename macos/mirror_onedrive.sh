7a45d282e545b82311a92090a7b74090a4543089
Abderrahmane TAHRI JOUTI <atahrijouti@paypal.com>
2023-10-18 08:48:06 +0200

    mac - mirror_ondrive

---
+++ macos/mirror_onedrive.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+SOURCE="/Users/atahrijouti/playground/logseq"
+TARGET="/Users/atahrijouti/OneDrive - PayPal/logseq"
+LOG_FILE="/Users/atahrijouti/.logs/mirror_onedrive/log.txt"
+
+RESULT=$(rsync -rau --delete --exclude=".git" "$SOURCE/" "$TARGET")
+
+if [[ $RESULT ]]; then
+  echo $RESULT >> "${LOG_FILE}"
+else
+  echo "$(tail -n 3000 "${LOG_FILE}")" > "${LOG_FILE}"
+  echo "Ran rsync $(date)" >> "${LOG_FILE}"
+fi
