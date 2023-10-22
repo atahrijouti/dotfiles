ee90c0ac111c370949a4cd66346b9378382da190
atj <Abderrahmane Tahri Jouti>
2023-10-22 14:16:46 +0200

    wezterm - silver-pond is windows

--- shell/wezterm/overrides/machine/silver-pond.lua
+++ shell/wezterm/overrides/machine/silver-pond.lua
@@ -1,11 +1,7 @@
 local M = {}
 
-local wezterm = require("wezterm")
-
 M.run = function(config)
-  if wezterm.target_triple == "x86_64-unknown-linux-gnu" then
-    config.font_size = 11
-  end
+  config.font_size = 11
 
   return config
 end
