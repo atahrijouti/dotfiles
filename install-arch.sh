e0f323c50c5b05fe0d4e78e2d1b438cd0eac17a1
Abderrahmane TAHRI JOUTI <302837+atahrijouti@users.noreply.github.com>
2024-02-24 16:52:22 +0100

    restructure arch installation script

--- install-arch.sh
+++ install-arch.sh
@@ -1,31 +1,5 @@
-# Update system
+# Install Packages
 sudo pacman -Suy
-sudo pacman -S zsh
-chsh -s $(which zsh)
-
-pacman -S --needed git base-devel
-
-mkdir ~/source && cd ~/source
-
-# Dotfiles
-git clone https://github.com/atahrijouti/dotfiles.git
-sh source/dotfiles/create-symlinks.sh
-
-# YAY
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
-cd
-
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-
-cd source
-git clone https://github.com/helix-editor/helix
-cd helix
-cargo install --path helix-term --locked
-cd
-
-
 sudo pacman -S linux-headers base-devel git github-cli intel-ucode pacman-contrib dkms zsh
 sudo pacman -S plasma-meta konsole kwrite dolphin ark plasma-wayland-session egl-wayland sddm pipewire pipewire-alsa pipewire-jack pipewire-pulse gst-plugin-pipewire libpulse wireplumber iio-sensor-proxy bluez bluez-utils powerdevil power-profiles-daemon cpupower 
 sudo pacman -S nano vim openssh htop wget iwd wireless_tools wpa_supplicant smartmontools xdg-utils lazygit fwupd wl-clipboard btop p7zip unrar tar rsync neofetch flac curl wget ufw flatpak samba 
@@ -33,15 +7,32 @@ sudo pacman -S exfat-utils fuse-exfat ntfs-3g
 sudo pacman -S ttf-jetbrains-mono-nerd noto-fonts-emoji noto-fonts ttf-scheherazade-new
 sudo pacman -S steam  wezterm lf fd fzf xclip wmctrl xdotool syncthing spectacle discord kodi retroarch retroarch-assets-xmb retroarch-assets-glui retroarch-assets-ozone libretro-core-info libretro-shaders-slang keepassxc
 
+mkdir ~/source && cd ~/source
+
+# Install AUR Packages
+git clone https://aur.archlinux.org/yay-bin.git
+cd yay-bin
+makepkg -si
 yay -Suy
-yay -S \
- google-chrome \
- protonup-qt \
- koi \
- informant \
- xone-dkms-git xone-dongle-firmware \
- kodi-addon-pvr-iptvsimple \
- moonlight-qt \
- appimagelauncher
+yay -S google-chrome protonup-qt koi informant appimagelauncher kodi-addon-pvr-iptvsimple 
+yay -S xone-dkms-git xone-dongle-firmware
+
+
+# Config
+chsh -s $(which zsh)
+
+# Dotfiles
+cd ~/source
+git clone https://github.com/atahrijouti/dotfiles.git
+sh source/dotfiles/create-symlinks.sh
+
+# Rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# Helix
+cd ~/source
+git clone https://github.com/helix-editor/helix
+cd helix
+cargo install --path helix-term --locked
 
 sudo usermod -a -G informant,render,video,audio,input,informant,wheel atj 
