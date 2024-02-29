72e622af3047156a970f5bd220b7932619626be0
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2024-02-29 23:19:38 +0100

    autologin

--- install-arch.sh
+++ install-arch.sh
@@ -5,7 +5,7 @@ sudo pacman -S plasma-meta konsole kwrite dolphin ark plasma-wayland-session egl
 sudo pacman -S nano vim openssh htop wget iwd wireless_tools wpa_supplicant smartmontools xdg-utils lazygit fwupd wl-clipboard btop p7zip unrar tar rsync neofetch flac curl wget ufw flatpak samba duf zsh-autosuggestions zsh-syntax-highlighting zsh-completions zsh-history-substring-search
 sudo pacman -S exfat-utils fuse-exfat ntfs-3g
 sudo pacman -S ttf-jetbrains-mono-nerd noto-fonts-emoji noto-fonts ttf-scheherazade-new
-sudo pacman -S steam  wezterm lf fd fzf xclip wmctrl xdotool syncthing spectacle discord kodi retroarch retroarch-assets-xmb retroarch-assets-glui retroarch-assets-ozone libretro-core-info libretro-shaders-slang keepassxc starship
+sudo pacman -S steam wezterm lf fd fzf xclip wmctrl xdotool syncthing spectacle discord kodi retroarch retroarch-assets-xmb retroarch-assets-glui retroarch-assets-ozone libretro-core-info libretro-shaders-slang keepassxc starship telegram-desktop
 
 mkdir ~/source && cd ~/source
 
@@ -17,7 +17,8 @@ yay -Suy
 yay -S google-chrome protonup-qt koi appimagelauncher kodi-addon-pvr-iptvsimple syncthingtray
 yay -S xone-dkms-git xone-dongle-firmware
 yay -S informant
-sudo usermod -a -G informant,render,video,audio,input,informant,wheel atj 
+
+sudo usermod -a -G informant,render,video,audio,input,informant,input,wheel atj 
 
 ## Rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
@@ -51,3 +52,17 @@ git config pull.rebase false
 
 gh auth login
 gh auth setup-git
+
+# mount network share
+# 
+# /etc/fstab
+# //<ip-address>/<share>   /mnt/<share-mount>    cifs    credentials=/etc/samba/credentials/<share-credentials>,iocharset=utf8,uid=uuu,gid=ggg,_netdev,nofail,noauto,x-systemd.automount     0 0
+
+
+# SDDM Autologin
+#
+# /etc/sddm.conf.d/autologin.conf
+#
+# [Autologin]
+# User=atj
+# Session=plasmawayland
