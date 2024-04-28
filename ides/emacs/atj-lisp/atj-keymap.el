9d13ec1d6b613f0f7bc58ca4800a3d343a322feb
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2024-04-28 20:50:14 +0200

    emacs - comment out general.el

--- ides/emacs/atj-lisp/atj-keymap.el
+++ ides/emacs/atj-lisp/atj-keymap.el
@@ -12,35 +12,10 @@
 (global-set-key (kbd "<escape>")      'keyboard-escape-quit)
 
 
-
-
 (defun atj/open-init-file ()
   "Open the init file."
   (interactive)
   (find-file user-init-file))
-
-
-
-
-
-
-;; (use-package general
-;;   :config
-;;   (general-evil-setup)
-
-;;   (general-create-definer atj/leader-keys
-;;     :states '(normal visual emacs)
-;;     :keymaps 'override
-;;     :prefix "SPC"
-;;   )
-
-;;   (atj/leader-keys
-;;    "w" '(save-buffer :wk "Write file")
-;;   )
-;; )
-
-
-
 
 
 (provide 'atj-keymap)
