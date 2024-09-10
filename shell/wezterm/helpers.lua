7bbd25ae6a745be57e35337e62adc328e176764c
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2024-09-10 09:51:49 +0200

    wezterm - use Ef-Maris

--- shell/wezterm/helpers.lua
+++ shell/wezterm/helpers.lua
@@ -1,39 +1,38 @@
 local wezterm = require("wezterm")
 
 _G.nocase = function(s)
-  s = string.gsub(s, "%a", function(c)
-    return string.format("[%s%s]", string.lower(c),
-      string.upper(c))
-  end)
-  return s
+	s = string.gsub(s, "%a", function(c)
+		return string.format("[%s%s]", string.lower(c), string.upper(c))
+	end)
+	return s
 end
 
 local M = {}
 
 M.decideColorScheme = function(config)
-  local appearance = "Dark"
+	local appearance = "Dark"
 
-  if wezterm.gui then
-    appearance = wezterm.gui.get_appearance()
-  end
+	if wezterm.gui then
+		appearance = wezterm.gui.get_appearance()
+	end
 
-  if appearance:find("Dark") then
-    return "tokyonight_storm"
-  else
-    config.colors = {
-      background = "#F2F3F7"
-    }
-    return "tokyonight_day"
-  end
+	if appearance:find("Dark") then
+		return "Ef-Maris-Dark"
+	else
+		config.colors = {
+			background = "#F2F3F7",
+		}
+		return "Ef-Maris-Light"
+	end
 end
 
 M.togglePadding = function(config)
-  config.window_padding = {
-    left = 10,
-    right = 10,
-    top = 10,
-    bottom = 10,
-  }
+	config.window_padding = {
+		left = 10,
+		right = 10,
+		top = 10,
+		bottom = 10,
+	}
 end
 
 return M
