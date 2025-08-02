f48fb1654652fdc408293c68e7022e316164f581
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2025-08-02 11:36:12 +0200

    emacs - trust default themes

--- ides/emacs/atj-lisp/atj-ui.el
+++ ides/emacs/atj-lisp/atj-ui.el
@@ -7,16 +7,18 @@
   :ensure t
   :diminish
   :config
+
   (setq custom-safe-themes
-    '(
-      "3b8713240ad8a387afc4ca9033d2df9abb2fd32b0ce09a4159169878067327e6"
-      "0b2c80b52629015e3b52d7406ffb0b82f9c10dce5113c9a46bb42653914f1d05"
-      default
-    )
+    '("dfcd2b13f10da4e5e26eb1281611e43a134d4400b06661445e7cbb183c47d2ec"
+     "3b8713240ad8a387afc4ca9033d2df9abb2fd32b0ce09a4159169878067327e6"
+     "0b2c80b52629015e3b52d7406ffb0b82f9c10dce5113c9a46bb42653914f1d05"
+     default)
   )
+  
   (setq auto-dark-themes '((doom-nord-aurora) (doom-nord-light)))
   (setq auto-dark-allow-osascript t)
-  (auto-dark-mode t))
+  (auto-dark-mode t)
+)
 
 ;; Icons everywhere
 (use-package all-the-icons
