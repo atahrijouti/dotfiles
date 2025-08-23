60394afa12c28acbde2749b7359ac9f609fa3fd7
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2025-08-23 18:04:46 +0200

    emacs - add safe theme

--- ides/emacs/custom-vars.el
+++ ides/emacs/custom-vars.el
@@ -5,7 +5,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("dfcd2b13f10da4e5e26eb1281611e43a134d4400b06661445e7cbb183c47d2ec"
+   '("0b2c80b52629015e3b52d7406ffb0b82f9c10dce5113c9a46bb42653914f1d05"
+	 "dfcd2b13f10da4e5e26eb1281611e43a134d4400b06661445e7cbb183c47d2ec"
 	 "87fa3605a6501f9b90d337ed4d832213155e3a2e36a512984f83e847102a42f4"
 	 default)))
 (custom-set-faces
