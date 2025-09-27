30f6a0c45de0d1067ff951fd828eac416f929ea5
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2025-09-27 12:48:41 +0200

    wezterm - ucrt64 zsh launcher

--- shell/wezterm/keymaps.lua
+++ shell/wezterm/keymaps.lua
@@ -10,8 +10,8 @@ M.run = function(config)
   config.leader = { key = ",", mods = "CTRL", timeout_milliseconds = 10000 }
 
   config.keys = {
-    { key = "9", mods = "ALT", action = wezterm.action_callback(mux.openLazyGitdown()) },
-    { key = "1", mods = "ALT", action = wezterm.action_callback(mux.openLfLeft()) },
+    { key = "9", mods = "ALT",    action = wezterm.action_callback(mux.openLazyGitdown()) },
+    { key = "1", mods = "ALT",    action = wezterm.action_callback(mux.openLfLeft()) },
 
     --
     -- panes
@@ -22,41 +22,45 @@ M.run = function(config)
       mods = "LEADER",
       action = act.SplitPane({ direction = "Right" }),
     },
-    { key = "z", mods = "LEADER", action = act.TogglePaneZoomState },
-    { key = "c", mods = "LEADER", action = act.CloseCurrentPane({ confirm = true }) },
+    { key = "z",   mods = "LEADER",     action = act.TogglePaneZoomState },
+    { key = "c",   mods = "LEADER",     action = act.CloseCurrentPane({ confirm = true }) },
+
+    -- tabs
+    { key = 'Tab', mods = 'CTRL|SHIFT', action = act.ActivateTabRelative(-1) },
+    { key = 'Tab', mods = 'CTRL',       action = act.ActivateTabRelative(1) },
 
     -- activate pane
-    { key = "h", mods = "SUPER|ALT", action = act.ActivatePaneDirection("Left") },
-    { key = "l", mods = "SUPER|ALT", action = act.ActivatePaneDirection("Right") },
-    { key = "k", mods = "SUPER|ALT", action = act.ActivatePaneDirection("Up") },
-    { key = "j", mods = "SUPER|ALT", action = act.ActivatePaneDirection("Down") },
+    { key = "h",   mods = "SUPER|ALT",  action = act.ActivatePaneDirection("Left") },
+    { key = "l",   mods = "SUPER|ALT",  action = act.ActivatePaneDirection("Right") },
+    { key = "k",   mods = "SUPER|ALT",  action = act.ActivatePaneDirection("Up") },
+    { key = "j",   mods = "SUPER|ALT",  action = act.ActivatePaneDirection("Down") },
 
     -- resize pane
-    { key = "H", mods = "SUPER|ALT", action = act.AdjustPaneSize({ "Left", 1 }) },
-    { key = "L", mods = "SUPER|ALT", action = act.AdjustPaneSize({ "Right", 1 }) },
-    { key = "K", mods = "SUPER|ALT", action = act.AdjustPaneSize({ "Up", 1 }) },
-    { key = "J", mods = "SUPER|ALT", action = act.AdjustPaneSize({ "Down", 1 }) },
-    { key = "p", mods = "LEADER", action = act.ActivateKeyTable({ name = "pane_selection" }) },
+    { key = "H",   mods = "SUPER|ALT",  action = act.AdjustPaneSize({ "Left", 1 }) },
+    { key = "L",   mods = "SUPER|ALT",  action = act.AdjustPaneSize({ "Right", 1 }) },
+    { key = "K",   mods = "SUPER|ALT",  action = act.AdjustPaneSize({ "Up", 1 }) },
+    { key = "J",   mods = "SUPER|ALT",  action = act.AdjustPaneSize({ "Down", 1 }) },
+    { key = "p",   mods = "LEADER",     action = act.ActivateKeyTable({ name = "pane_selection" }) },
 
     -- Clipboard
     -- { key = "C", mods = "CTRL", action = act.CopyTo("Clipboard") },
-    { key = "V", mods = "CTRL", action = act.PasteFrom("Clipboard") },
-    { key = "x", mods = "LEADER", action = wezterm.action.ActivateCopyMode },
-    { key = "w", mods = "LEADER", action = wezterm.action.QuickSelect },
+    { key = "V",   mods = "CTRL",       action = act.PasteFrom("Clipboard") },
+    { key = "x",   mods = "LEADER",     action = wezterm.action.ActivateCopyMode },
+    { key = "w",   mods = "LEADER",     action = wezterm.action.QuickSelect },
 
     -- Search
-    { key = "f", mods = "LEADER", action = act.Search("CurrentSelectionOrEmptyString") },
+    { key = "f",   mods = "LEADER",     action = act.Search("CurrentSelectionOrEmptyString") },
 
     -- spawn
-    { key = "t", mods = "LEADER", action = act.SpawnTab("CurrentPaneDomain") },
-    { key = "n", mods = "LEADER", action = act.SpawnWindow },
+    { key = "t",   mods = "LEADER",     action = act.SpawnTab("CurrentPaneDomain") },
+    { key = "n",   mods = "LEADER",     action = act.SpawnWindow },
 
     -- appliation
-    { key = "q", mods = "LEADER", action = act.QuitApplication },
-    { key = "h", mods = "LEADER", action = act.Hide },
-    { key = "r", mods = "LEADER", action = act.ReloadConfiguration },
-    { key = ",", mods = "LEADER", action = act.ActivateCommandPalette },
-    { key = ";", mods = "LEADER", action = act.ShowDebugOverlay },
+    { key = "q",   mods = "LEADER",     action = act.QuitApplication },
+    { key = "h",   mods = "LEADER",     action = act.Hide },
+    { key = "r",   mods = "LEADER",     action = act.ReloadConfiguration },
+    { key = ",",   mods = "LEADER",     action = act.ActivateCommandPalette },
+    { key = ";",   mods = "LEADER",     action = act.ShowDebugOverlay },
     {
       key = "l",
       mods = "LEADER",
--- shell/wezterm/overrides/os/windows.lua
+++ shell/wezterm/overrides/os/windows.lua
@@ -3,6 +3,15 @@ local M = {}
 M.run = function(config)
   config.font_size = 11
   config.default_prog = { "cmd.exe", "/k" }
+
+  local launch_menu = {}
+
+  table.insert(launch_menu, {
+    label = "MSYS UCRT64",
+    args = { "cmd.exe ", "/k", "C:\\msys64\\msys2_shell.cmd -defterm -here -no-start -ucrt64 -shell zsh" },
+  })
+
+  config.launch_menu = launch_menu
   return config
 end
 
