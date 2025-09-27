c718a50097aac6172ae4612c4ba30563367ab1c4
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2025-09-27 13:53:37 +0200

    windows - make ucrt64 zsh default

--- shell/wezterm/overrides/os/windows.lua
+++ shell/wezterm/overrides/os/windows.lua
@@ -2,13 +2,18 @@ local M = {}
 
 M.run = function(config)
   config.font_size = 11
-  config.default_prog = { "cmd.exe", "/k" }
+  config.default_prog = { "cmd.exe ", "/k", "C:\\msys64\\msys2_shell.cmd -defterm -here -no-start -ucrt64 -shell zsh" }
 
   local launch_menu = {}
 
   table.insert(launch_menu, {
-    label = "MSYS UCRT64",
+    label = "ZSH - MSYS UCRT64",
     args = { "cmd.exe ", "/k", "C:\\msys64\\msys2_shell.cmd -defterm -here -no-start -ucrt64 -shell zsh" },
+  })
+
+  table.insert(launch_menu, {
+    label = "Clink CMD",
+    args = { "cmd.exe ", "/k" },
   })
 
   config.launch_menu = launch_menu
