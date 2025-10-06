3af3bb670f0f0c26edaa95050022640a253181c8
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2025-10-06 16:27:16 +0200

    clink - no transient

--- windows/clink/starship.lua
+++ windows/clink/starship.lua
@@ -1,7 +1,1 @@
-function starship_transient_prompt_func(prompt)
-    return io.popen("starship module character"
-        .. " --keymap=" .. rl.getvariable('keymap')
-    ):read("*a")
-end
-
 load(io.popen("starship init cmd"):read("*a"))()
