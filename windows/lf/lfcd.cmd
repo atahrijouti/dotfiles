d13379370794b5a93ac541e618211b52a7b8d11f
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2025-10-10 19:42:33 +0200

    windows - remove old old files

--- windows/lf/lfcd.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-rem Change working dir in cmd.exe to last dir in lf on exit.
-rem
-rem You need to put this file to a folder in %PATH% variable.
-
-for /f "usebackq tokens=*" %%d in (`lf -print-last-dir %*`) do cd %%d
