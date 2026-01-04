d49663c81a6ac7a7de47c972033df82365020d25
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2026-01-04 17:29:02 +0100

    windows: no-hdr script

--- windows/scripts/1080p-no-hdr.ps1
+++ windows/scripts/1080p-no-hdr.ps1
@@ -1,4 +1,5 @@
 Import-Module WindowsDisplayManager
 $primaryDisplay = WindowsDisplayManager\GetPrimaryDisplay
 
-Write-Host "Update Successful?: $($primaryDisplay.SetResolution(1920,1080,60) -and $primaryDisplay.DisableHdr())"
+$primaryDisplay.SetResolution(1920,1080,60)
+$primaryDisplay.DisableHdr()
--- windows/scripts/3440-1440-no-hdr.ps1
+++ windows/scripts/3440-1440-no-hdr.ps1
@@ -2,4 +2,5 @@ Import-Module WindowsDisplayManager
 $primaryDisplay = WindowsDisplayManager\GetPrimaryDisplay
 
 $primaryDisplay.SetResolution(3440,1440,144)
-$primaryDisplay.EnableHdr()
+$primaryDisplay.DisableHdr()
+ 
