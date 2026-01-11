455d58602068d417a95d2457e2290ccadda0dea2
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2026-01-11 15:19:46 +0100

    koi - re-use nu's command

--- scripts/switch_dark.sh
+++ scripts/switch_dark.sh
@@ -1,14 +1,3 @@
-#!/bin/sh
+#!/bin/bash
 
-UNAME="$(uname -s)"
-case "$UNAME" in
-  MSYS*|MINGW*|CYGWIN*)  # Windows (MSYS2)
-    HELIX_CONFIG="$HOME/AppData/Roaming/helix"
-    ;;
-  *)
-    HELIX_CONFIG="$HOME/.config/helix"
-    ;;
-esac
-
-# apply sed
-sed -i -E "s/(inherits\s=\s\").*(\")/\1my_jetbrains_dark\2/" "$HELIX_CONFIG/themes/site-theme.toml"
+nu -l -c "scripts switch-helix-theme dark"
