d85459ed1814f18d17aeef0407a3a5775c4c50a6
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2026-09-26 10:12:25 +0200

    not needed

--- find_symlinks.sh
+++
@@ -1,8 +0,0 @@
-find ~ -type l -exec sh -c '
-  for link; do
-    target=$(readlink -f "$link") || continue
-    case "$target" in
-      /home/atj/source/dotfiles/*) echo "$link -> $target" ;;
-    esac
-  done
-' sh {} +
